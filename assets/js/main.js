98f0f92f3764cf1ed685a905780fd60136cad96f
Giuditta <giudittamarino89@gmail.com>
2021-03-24 15:31:59 +0100

    html-css

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+var app = new Vue (
+  {
+    el: '#root',
+    data: {
+      inedx: 0,
+      arrayImg: [
+        './assets/img/eraserhead.jpg',
+        './assets/img/blue_velvet.jpg',
+        './assets/img/fire_walk_with_me.jpg',
+        './assets/img/mulholland_drive.jpg',
+        './assets/img/inland_empire.jpg'
+      ]
+
+    }
+});
